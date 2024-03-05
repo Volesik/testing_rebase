98628b97e47d766a4d2a6b1b58215822c03a834c
Viktor Kyselov <dnepr613@gmail.com>
2024-03-05 22:48:42 +0200

    Init items.

---
+++ test_solution/rebaseApi/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace rebaseApi.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ test_solution/rebaseApi/Seeding/SeedingItem.cs
@@ -0,0 +1,14 @@
+﻿using rebaseApi.Models;
+
+namespace rebaseApi.Seeding
+{
+    public class SeedingItem
+    {
+        public IEnumerable<Item> ReturnItems()
+        {
+            yield return new Item { Id = 1, Name = "Name1", Email = "email1@fake.com" };
+            yield return new Item { Id = 2, Name = "Name2", Email = "email2@fake.com" };
+            yield return new Item { Id = 3, Name = "Name3", Email = "email3@fake.com" };
+        }
+    }
+}
